c42de1ec96e06c5888518130124d04d30d8e5189
QSDR2s1d <123747570+QSDR2s1d@users.noreply.github.com>
2025-09-24 18:30:54 +0800

    Update title.py

--- data/python/title.py
+++ data/python/title.py
@@ -20,7 +20,7 @@ for file_path in file_list:
 
     # 在文件顶部插入内容
     new_content = f"[Adblock Plus 2.0]\n" \
-                  f"! Title: HyperADRules\n" \
+                  f"! Title: Ad_Rules\n" \
                   f"! Homepage: https://github.com/QSDR2s1d/Ad_Rules\n" \
                   f"! Expires: 12 Hours\n" \
                   f"! Version: {beijing_time}（北京时间）\n" \
